00a28f2ef3a7e0a3e7ad5bbea5cd03d1250dc679
naoygzn21-gif <nao.ygzn21@gmail.com>
2026-03-29 21:25:09 +0900

    Add files via upload

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 /* ===================================================
    data.js — おうちごはん帖 共有データファイル
    管理画面の「データを保存」ボタンで上書き更新します
-   更新日時: 2026/3/29 21:14:19
+   更新日時: 2026/3/29 21:24:23
    =================================================== */
 SITE_DATA = {
   "adminEmail": "nao.ygzn21@gmail.com",
   "adminPw": "kaikuu08",
-  "themeCss": ":root {\n    --bg:#fffbf4;--paper:#ffffff;--paper2:#ffff9d;\n    --ink:#2a3d28;--ink-mid:#69bfb2;--ink-lt:#91e7da;\n    --accent:#eaa8a4;--accent-lt:#fff8f4;--accent-md:#e1da75;\n    --sub:#69bfb2;--border:#a5fbee;--border2:#69bfb2;\n    --rule:#affff8;--tape:#69bfb266;\n  }",
+  "themeCss": ":root {\n    --bg:#fffbf4;--paper:#ffffff;--paper2:#ffffb1;\n    --ink:#2a3d28;--ink-mid:#4A69B3;--ink-lt:#7291db;\n    --accent:#BA3801;--accent-lt:#ff8851;--accent-md:#FFEC89;\n    --sub:#4A69B3;--border:#86a5ef;--border2:#4A69B3;\n    --rule:#90aff9;--tape:#4A69B366;\n  }",
   "sections": {
     "videos": true,
     "gallery": true,
